259f0a0fe89b4bc06d61666f8dcefd43bf8896b4
Miguel Gómez <145044778+MGrammer02@users.noreply.github.com>
2026-01-13 09:43:16 -0500

    act

--- packages/lib/OgImages.tsx
+++ packages/lib/OgImages.tsx
@@ -52,21 +52,21 @@ const makeAbsoluteUrl = (url: string) =>
 
 const OG_ASSETS = {
   meeting: {
-    id: "bizcal-meeting-og-v1", // Bump version when changing Meeting component structure/styling
+    id: "meeting-og-image-v2", // Bump version when changing Meeting component structure/styling
     logo: LOGO,
     logoWidth: "350",
     avatarSize: "160",
     variant: "dark" as const,
   },
   app: {
-    id: "bizcal-og-image-v1", // Bump version when changing App component structure/styling
+    id: "app-og-image-v2", // Bump version when changing App component structure/styling
     logo: LOGO,
     logoWidth: "150",
     iconSize: "172",
     variant: "light" as const,
   },
   generic: {
-    id: "bizcal-og-image-v1", // Bump version when changing Generic component structure/styling
+    id: "generic-og-image-v2", // Bump version when changing Generic component structure/styling
     logo: "cal-logo-word-black.svg",
     logoWidth: "350",
     variant: "light" as const,
